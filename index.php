98782aa4e66024bd434ba200ac8bbc9e463cd813
FURY438 <68443707+FURY438@users.noreply.github.com>
2022-11-15 15:42:54 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,106 +5,65 @@ echo '<img src="Screenshot_2.png"/>';
 echo '<br>';
 $myArr=array(array(-1.2,13.8,-5.2),array(6.1,-3.2,12.1),array(2.2,-4.2,7.1));
 echo '<h4>Наш масив: </h4>';
-
-for($row=0;$row<=count($myArr)-1;$row++){
-    for($col=0;$col<=count($myArr[$row])-1;$col++){
-        echo "{$myArr[$row][$col]}; ";
-    }
-    echo "<br>";
-}
+print_r($myArr);
 ?>
 <div>
     <?php
-    $oneArr=array();
-    $negativeElArr=array();
-    for($row=0;$row<=count($myArr)-1;$row++) {
-        for ($col = 0; $col <= count($myArr[$row]) - 1; $col++) {
-            if($myArr[$row][$col]<0)
-            $negativeElArr[] = $myArr[$row][$col];
-            $oneArr[] = $myArr[$row][$col];
-        }
+    for($row=0;$row<count($myArr);$row++){
+    $negativeElArr[]=array_filter($myArr[$row],function ($n){if($n<0) return (true);});
     }
 
-    if($maxNegativeEl=max($negativeElArr)) echo "<br>Максимальний від'мний елемент: {$maxNegativeEl}";
-    else echo "<p>В даному масиві немає від'ємних елементів</p>";
+    for($row=0;$row<count($negativeElArr);$row++) {
+        $maxNegativeElArr[] = max($negativeElArr[$row]);
+}
+
+    $maxNegativeEl=max($maxNegativeElArr);
+    echo "<br>Максимальний від'мний елемент: {$maxNegativeEl}";
     ?>
 </div>
 <div>
     <?php
-    $maxEl=max($oneArr);
-    $newArr=array();
-    $newArr=$myArr;
+    for($row=0;$row<count($myArr);$row++) {
+        $maxEllArr[] = max($myArr[$row]);
+    }
+    $maxEl=max($maxEllArr);
+    for($row=0;$row<count($myArr);$row++){
+        $myArr2[]=array_map(function ($n){ global $maxEl; return ($n*$maxEl);},$myArr[$row]);
+    }
     echo "<h4>Нова матриця елементи якої рівні добутку елементів старої матриці на максимальний елемент матриці</h4>";
-    for($row=0;$row<=count($newArr)-1;$row++){
-        for($col=0;$col<=count($newArr[$row])-1;$col++){
-            $newArr[$row][$col]*=$maxEl;
-            echo "{$newArr[$row][$col]}; ";
-        }
-        echo "<br>";
-    }
+    print_r($myArr2);
     ?>
 </div>
 <div>
     <?php
-    $oneArr2=array();
-    for($row=0;$row<=count($newArr)-1;$row++) {
-        for ($col = 0; $col <= count($newArr[$row]) - 1; $col++) {
-            $oneArr2[] = $newArr[$row][$col];
-        }
+    for($row=0;$row<count($myArr2);$row++){
+        $positivElArr[]=array_filter($myArr2[$row],function ($n){if($n>=0) return (true);});
     }
-    $sumArr2=0;
-    $multArr2=1;
-    for($row=0;$row<=count($newArr)-1;$row++) {
-        for ($col = 0; $col <= count($newArr[$row]) - 1; $col++) {
-            if($newArr[$row][$col]>=0){
-                $sumArr2+=$newArr[$row][$col];
-                $multArr2=$newArr[$row][$col];
-            }
-        }
+
+    for($row=0;$row<count($positivElArr);$row++) {
+    $sumEllArr[] = array_sum($positivElArr[$row]);
+    $multEllArr[]=array_product($positivElArr[$row]);
     }
+    $sumArr2=array_sum($sumEllArr);
+    $multArr2=array_product($multEllArr);
+
     echo "<br>Сума додатніх елементів масиву: {$sumArr2}";
     echo "<br>Добуток додатных елементыв масиву: {$multArr2}";
+    if($sumArr2>$multArr2) echo "<br>Сума більша від добутку";
+    else echo "<br>добуток більший від суми";
     ?>
+
 </div>
 <div>
     <?php
-    $positionNegEl=array();
-    echo "<p>Координати від'ємних елементів матриці</p>";
-    for($row=0;$row<=count($newArr)-1;$row++) {
-        for ($col = 0; $col <= count($newArr[$row]) - 1; $col++) {
-            if($newArr[$row][$col]<0 ){
-            $positionNegEl[]=array($row,$col);
-            }
-        }
+   echo "Координати від'ємних елементів масиву";
+    for($row=0;$row<count($myArr2);$row++){
+        $negativeElArr2[]=array_filter($myArr2[$row],function ($n){if($n<0) return (true);});
+        $zeroElArr2[]=array_filter($myArr2[$row],function ($n){if($n==0) return (true);});
     }
-    for($row=0;$row<=count($positionNegEl)-1;$row++){
-        for($col=0;$col<=count($positionNegEl[$row])-1;$col++){
-            echo "[{$positionNegEl[$row][$col]}]; ";
-        }
-        echo "<br>";
-    }
-    ?>
-</div>
-<div>
-    <?php
-    $positionZeroEl=array();
-    echo "<p>Координати нульових елементів матриці</p>";
-    for($row=0;$row<=count($newArr)-1;$row++) {
-        for ($col = 0; $col <= count($newArr[$row]) - 1; $col++) {
-            if($newArr[$row][$col]= 0 ){
-                $positionZeroEl[]=array($row,$col);
-            }
-        }
-    }
-   if(count($positionZeroEl)>0) {
-       for ($row = 0; $row <= count($positionNegEl) - 1; $row++) {
-           for ($col = 0; $col <= count($positionNegEl[$row]) - 1; $col++) {
-               echo "[{$positionNegEl[$row][$col]}]; ";
-           }
-           echo "<br>";
-       }
-   }
-   else
-       echo "<p>Нулових елементів в матриці немає!!!!</p>";
+    print_r($negativeElArr2);
+    echo "<br>";
+    if(array_values($zeroElArr2))echo "<br>Нульових елементів немає";
+    else print_r($zeroElArr2);
     ?>
 </div>
